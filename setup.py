79a787ac6b65757c367e8e078a0a8771a4408ab5
eunwoo1104 <sions04@naver.com>
2020-12-23 14:29:57 +0900

    Version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as f:
 
 setuptools.setup(
     name="discord-py-slash-command",
-    version="1.0.5",
+    version="1.0.6",
     author="eunwoo1104",
     author_email="sions04@naver.com",
     description="Simple Discord Slash Command extension for discord.py.",
