fe5cfc9cce0a3f5cc4e974734bed618f6ef82002
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-03-04 19:10:57 +0900

    remove star import

--- discord/ext/slash.py
+++ discord/ext/slash.py
@@ -1,1 +1,5 @@
-from discord_slash import *
+from discord_slash.client import SlashCommand
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.context import SlashContext
+from discord_slash.utils import manage_commands
+from discord_slash import client, cog_ext, context, error, http, model
