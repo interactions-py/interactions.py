4fd02ff0b950b2ba981e99fb08aaa62fefec5f64
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-05-11 12:55:07 +0800

    Update discord_slash/utils/manage_commands.py
    Co-authored-by: Sam <anothercat1259@gmail.com>

--- discord_slash/utils/manage_commands.py
+++ discord_slash/utils/manage_commands.py
@@ -345,7 +345,7 @@ def create_multi_ids_permission(ids: typing.List[int], id_type: int, permission:
 
     :param id: List of target ids to apply the permission on.
     :param id_type: Type of the id. 
-    :param permission: State of the permission. ``True`` to allow access, ``False to disallow access.
+    :param permission: State of the permission. ``True`` to allow access, ``False`` to disallow access.
     """
     return [create_permission(id, id_type, permission) for id in set(ids)]
 
