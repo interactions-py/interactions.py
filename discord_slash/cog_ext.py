4b149ecbc9293a2f595c72c3387bf46f18af7ca3
DeltaXWizard <33706469+deltaxwizard@users.noreply.github.com>
2021-06-12 13:04:14 -0400

    Reorder logic due to iterating a None object.

--- discord_slash/cog_ext.py
+++ discord_slash/cog_ext.py
@@ -50,8 +50,9 @@ def cog_slash(*,
         else:
             opts = options
 
-        if not all(isinstance(item, int) for item in guild_ids) and guild_ids is not []:
-            raise Exception(f"The snowflake IDs {guild_ids} given are not a list of integers. Because of discord.py convention, please use integer IDs instead. Furthermore, the command '{name or cmd.__name__}' will be deactivated and broken until fixed.")
+        if guild_ids is not None:
+            if not all(isinstance(item, int) for item in guild_ids):
+                raise Exception(f"The snowflake IDs {guild_ids} given are not a list of integers. Because of discord.py convention, please use integer IDs instead. Furthermore, the command '{name or cmd.__name__}' will be deactivated and broken until fixed.")
 
         _cmd = {
             "func": cmd,
@@ -133,8 +134,9 @@ def cog_subcommand(*,
         else:
             opts = options
 
-        if not all(isinstance(item, int) for item in guild_ids) and guild_ids is not []:
-            raise Exception(f"The snowflake IDs {guild_ids} given are not a list of integers. Because of discord.py convention, please use integer IDs instead. Furthermore, the command '{name or cmd.__name__}' will be deactivated and broken until fixed.")
+        if guild_ids is not None:
+            if not all(isinstance(item, int) for item in guild_ids):
+                raise Exception(f"The snowflake IDs {guild_ids} given are not a list of integers. Because of discord.py convention, please use integer IDs instead. Furthermore, the command '{name or cmd.__name__}' will be deactivated and broken until fixed.")
 
         _cmd = {
             "func": None,
