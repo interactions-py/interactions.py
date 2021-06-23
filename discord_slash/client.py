c84d301ea9d5e3b908d713c3309c9b100ddf885a
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-06-23 20:29:15 +0800

    Fix error when guild id is None.

--- discord_slash/client.py
+++ discord_slash/client.py
@@ -535,7 +535,7 @@ class SlashCommand:
         """
         name = name or cmd.__name__
         name = name.lower()
-        if not all(isinstance(item, int) for item in guild_ids):
+        if guild_ids and not all(isinstance(item, int) for item in guild_ids):
             raise error.IncorrectGuildIDType(
                 f"The snowflake IDs {guild_ids} given are not a list of integers. Because of discord.py convention, please use integer IDs instead. Furthermore, the command '{name}' will be deactivated and broken until fixed."
             )
