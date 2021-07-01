e4c7db88e97420973ba72520633d39f9cb354bf7
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-01 22:30:38 +0800

    Implement permissions decorator for cogs. (#237)

--- discord_slash/client.py
+++ discord_slash/client.py
@@ -876,11 +876,11 @@ class SlashCommand:
     def permission(self, guild_id: int, permissions: list):
         """
         Decorator that add permissions. This will set the permissions for a single guild, you can use it more than once for each command.
+
         :param guild_id: ID of the guild for the permissions.
         :type guild_id: int
-        :param permissions: Permission requirements of the slash command. Default ``None``.
-        :type permissions: dict
-
+        :param permissions: List of permissions to be set for the specified guild.
+        :type permissions: list
         """
 
         def wrapper(cmd):
