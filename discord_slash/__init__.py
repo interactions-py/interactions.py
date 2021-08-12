bffad29d5613d68157a85fb03c4c107412e1cf2c
goverfl0w <officialdeltadev@gmail.com>
2021-08-11 22:03:55 -0400

    Ignore model error

--- discord_slash/__init__.py
+++ discord_slash/__init__.py
@@ -10,6 +10,6 @@ from .client import SlashCommand  # noqa: F401
 from .const import __version__  # noqa: F401
 from .context import ComponentContext, MenuContext, SlashContext  # noqa: F401
 from .dpy_overrides import ComponentMessage  # noqa: F401
-from .model import ButtonStyle, ComponentType, SlashCommandOptionType  # noqa: F401
+from .model import ButtonStyle, ComponentType, SlashCommandOptionType, ContextMenuType  # noqa: F401
 from .utils import manage_commands  # noqa: F401
 from .utils import manage_components  # noqa: F401
