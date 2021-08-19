d3545c060f35c56603d635cd32d3fc32ec877f66
DeltaXWizard <33706469+deltaxwizard@users.noreply.github.com>
2021-08-19 12:43:48 -0400

    Include Channel and Guild Type codes

--- dis_interact/types/enums.py
+++ dis_interact/types/enums.py
@@ -82,6 +82,58 @@ class HTTPResponse(IntEnum):
     GATEWAY_UNAVAILABLE = 502
 
 
+class Channel(IntEnum):
+    """
+    Types of channels.
+
+    ..note::
+        While all of them are listed, not all of them would be used at this lib's scope.
+    """
+
+    GUILD_TEXT = 0
+    DM = 1
+    GUILD_VOICE = 2
+    GROUP_DM = 3
+    GUILD_CATEGORY = 4
+    GUILD_NEWS = 5
+    GUILD_STORE = 6
+    GUILD_NEWS_THREAD = 10
+    GUILD_PUBLIC_THREAD = 11
+    GUILD_PRIVATE_THREAD = 12
+    GUILD_STAGE_VOICE = 13
+
+
+class Message(IntEnum):
+    """Type of messages.
+
+    ..note::
+        While all of them are listed, not all of them would be used at this lib's scope.
+    """
+
+    DEFAULT = 0
+    RECIPIENT_ADD = 1
+    RECIPIENT_REMOVE = 2
+    CALL = 3
+    CHANNEL_NAME_CHANGE = 4
+    CHANNEL_ICON_CHANGE = 5
+    CHANNEL_PINNED_MESSAGE = 6
+    GUILD_MEMBER_JOIN = 7
+    USER_PREMIUM_GUILD_SUBSCRIPTION = 8
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1 = 9
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2 = 10
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3 = 11
+    CHANNEL_FOLLOW_ADD = 12
+    GUILD_DISCOVERY_DISQUALIFIED = 14
+    GUILD_DISCOVERY_REQUALIFIED = 15
+    GUILD_DISCOVERY_GRACE_PERIOD_INITIAL_WARNING = 16
+    GUILD_DISCOVERY_GRACE_PERIOD_FINAL_WARNING = 17
+    THREAD_CREATED = 18
+    REPLY = 19
+    APPLICATION_COMMAND = 20
+    THREAD_STARTER_MESSAGE = 21
+    GUILD_INVITE_REMINDER = 22
+
+
 class Options(IntEnum):
     """
     Enumerable object of literal integers holding equivocal values of a slash command's option(s).
@@ -138,7 +190,7 @@ class Options(IntEnum):
                     # proven in 3.7.8+, 3.8.6+, 3.9+ definitively
                     return cls.MENTIONABLE
         if not hasattr(typing, "_GenericAlias"):  # py 3.6
-            if isinstance(t, typing._Union):  # noqa
+            if isinstance(_type, typing._Union):  # noqa
                 return cls.MENTIONABLE
 
         if issubclass(_type, float):  # Python floats are essentially doubles, compared to languages when it's separate.
