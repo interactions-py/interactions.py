c846d6fc33757e8415ac5e0b436a21f0123ca865
DeltaXWizard <33706469+deltaxwizard@users.noreply.github.com>
2021-08-19 14:49:34 -0400

    Add intents + an .default()/.all() func

--- dis_interact/types/enums.py
+++ dis_interact/types/enums.py
@@ -134,6 +134,65 @@ class Message(IntEnum):
     GUILD_INVITE_REMINDER = 22
 
 
+class Intents(IntEnum):
+    """
+    Intent flags defined by the Discord API.
+    """
+
+    # Base intents, exactly from the API
+    GUILDS = 1 << 0
+    GUILD_MEMBERS = 1 << 1
+    GUILD_BANS = 1 << 2
+    GUILD_EMOJIS_AND_STICKERS = 1 << 3
+    GUILD_INTEGRATIONS = 1 << 4
+    GUILD_WEBHOOKS = 1 << 5
+    GUILD_INVITES = 1 << 6
+    GUILD_VOICE_STATES = 1 << 7
+    GUILD_PRESENCES = 1 << 8
+    GUILD_MESSAGES = 1 << 9
+    GUILD_MESSAGE_REACTIONS = 1 << 10
+    GUILD_MESSAGE_TYPING = 1 << 11
+    DIRECT_MESSAGES = 1 << 12
+    DIRECT_MESSAGE_REACTIONS = 1 << 13
+    DIRECT_MESSAGE_TYPING = 1 << 14
+
+    # Shortcuts/grouping/aliases
+    DEFAULT = (
+            GUILDS
+            | GUILD_BANS
+            | GUILD_EMOJIS_AND_STICKERS
+            | GUILD_INTEGRATIONS
+            | GUILD_WEBHOOKS
+            | GUILD_INVITES
+            | GUILD_VOICE_STATES
+            | GUILD_MESSAGES
+            | GUILD_MESSAGE_REACTIONS
+            | GUILD_MESSAGE_TYPING
+            | DIRECT_MESSAGES
+            | DIRECT_MESSAGE_REACTIONS
+            | DIRECT_MESSAGE_TYPING
+    )
+
+    MESSAGES = GUILD_MESSAGES | DIRECT_MESSAGES
+    REACTIONS = GUILD_MESSAGE_REACTIONS | DIRECT_MESSAGE_REACTIONS
+    TYPING = GUILD_MESSAGE_TYPING | DIRECT_MESSAGE_TYPING
+
+    PRIVILEGED = GUILD_PRESENCES | GUILD_MEMBERS
+
+    NONE = 0  # No intents
+    ALL = DEFAULT | PRIVILEGED  # All intents are the regular + "requested" intents.
+
+    @classmethod
+    def all(cls) -> int:
+        """Returns all of the intents."""
+        return cls.ALL
+
+    @classmethod
+    def default(cls) -> int:
+        """Returns the default intents."""
+        return cls.DEFAULT
+
+
 class Options(IntEnum):
     """
     Enumerable object of literal integers holding equivocal values of a slash command's option(s).
