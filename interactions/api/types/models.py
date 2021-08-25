dd83108eb920afbbd2ac84919a66ea9aa78be52a
DeltaXWizard <33706469+DeltaXWizard@users.noreply.github.com>
2021-08-25 14:13:56 -0400

    Add more Message-related, Threads, Member, Permissions, Attachments and some Mention models

--- interactions/api/types/models.py
+++ interactions/api/types/models.py
@@ -2,8 +2,38 @@ import datetime
 from typing import Optional, Union, List
 
 
+# TODO: Reorganise these models based on which big obj uses little obj
+# TODO: Figure out ? placements.
+
+
+class MessageActivity(object):
+    type: int
+    party_id: Optional[str]
+
+
+class MessageReference(object):
+    message_id: Optional[int]
+    channel_id: Optional[int]
+    guild_id: Optional[int]
+    fail_if_not_exists: Optional[bool]
+
+
+class ThreadMetadata(object):
+    archived: bool
+    auto_archive_duration: int
+    archive_timestamp: datetime.datetime.timestamp
+    locked: bool
+    invitable: Optional[bool]
+
+
+class ThreadMember(object):
+    id: Optional[int]  # intents
+    user_id: Optional[int]
+    join_timestamp: datetime.datetime.timestamp
+    flags: int
+
+
 class User(object):
-    # TODO: Figure out ? placements.
     id: int
     username: str
     discriminator: str
@@ -23,6 +53,30 @@ class User(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+
+class Member(object):
+    """Also, the guild member obj (Or partial.)"""
+
+    user: Optional[User]
+    nick: Optional[str]
+    roles: List[str]
+    joined_at: datetime.datetime.timestamp
+    premium_since: Optional[datetime.datetime.timestamp]
+    deaf: bool
+    mute: bool
+    pending: Optional[bool]
+    permissions: Optional[str]
+
+
+class Overwrite(object):
+    """This is used for the PermissionOverride obj"""
+
+    id: int
+    type: int
+    allow: str
+    deny: str
+
+
 class Channel(object):
     """
     The big Channel model.
@@ -35,7 +89,7 @@ class Channel(object):
     type: int
     guild_id: Optional[int]
     position: Optional[int]
-    # ?? permission overwrite
+    permission_overwrites: List[Overwrite]
     name: str  # This apparently exists in DMs. Untested in v9, known in v6
     topic: Optional[str]
     nsfw: Optional[bool]
@@ -43,7 +97,7 @@ class Channel(object):
     bitrate: Optional[int]  # not really needed in our case
     user_limit: Optional[int]
     rate_limit_per_user: Optional[int]
-    recipients = Optional[List[User]]
+    recipients: Optional[List[User]]
     icon: Optional[str]
     owner_id: Optional[int]
     application_id: Optional[int]
@@ -53,12 +107,24 @@ class Channel(object):
     video_quality_mode: Optional[int]
     message_count: Optional[int]
     member_count: Optional[int]
-    # Thread attributes? not implemented yet
+    thread_metadata: Optional[ThreadMetadata]
+    member: Optional[ThreadMember]
     default_auto_archive_duration: Optional[int]
     permissions: Optional[str]
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+
+class Attachment(object):
+    id: int
+    filename: str
+    content_type: Optional[str]
+    size: int
+    url: str
+    proxy_url: str
+    height: Optional[int]
+    width: Optional[int]
 
 
 class Message(object):
@@ -71,31 +137,30 @@ class Message(object):
     id: int
     channel_id: int
     guild_id: Optional[int]
-    # author, its a User object
-    author = User
-    # member, guild member obj
+    author: User
+    member: Optional[Member]
     content: str
     timestamp: datetime.datetime.timestamp
     edited_timestamp: Optional[datetime.datetime.timestamp]
     tts: bool
     mention_everyone: bool
-    # mentions = array of Users, and maybe partial members
-    # mention roles
-    mention_roles = Optional[List[str]]
-    # mention_channels
-    # attachments
+    # mentions: array of Users, and maybe partial members
+    mentions = Optional[List[Union[Member, User]]]
+    mention_roles: Optional[List[str]]
+    mention_channels: Optional[List["ChannelMention"]]
+    attachments: Optional[List[Attachment]]
     # embeds
-    # reactions
+    reactions: Optional[List["ReactionObject"]]
     nonce: Union[int, str]
     pinned: bool
     webhook_id: Optional[int]
     type: int
-    # activity
+    activity: Optional[MessageActivity]
     # application
-    application_id = int
-    # message_reference
+    application_id: int
+    message_reference: Optional[MessageReference]
     flags: int
-    # reference: Message ???
+    referenced_message: Optional["Message"]  # pycharm says it works, idk
     # interaction
     thread: Optional[Channel]
 
@@ -110,17 +175,21 @@ class Message(object):
 class Emoji(object):
     id: Optional[int]
     name: Optional[str]
-    roles = Optional[List[str]]
+    roles: Optional[List[str]]
     user: Optional[User]
     require_colons: Optional[bool]
     managed: Optional[bool]
     animated: Optional[bool]
     available: Optional[bool]
 
-    def __init__(self, **kwargs) -> None:
-        for inst in self:
-            for kwarg in kwargs:
-                exec(f"self.{inst} = {kwarg}")
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class ReactionObject(object):
+    count: int
+    me: bool
+    emoji: Emoji
 
 
 class RoleTags(object):
@@ -147,3 +216,8 @@ class Role(object):
         self.__dict__.update(kwargs)
 
 
+class ChannelMention(object):
+    id: int
+    guild_id: int
+    type: int  # Replace with enum from Channel Type, another PR
+    name: str
