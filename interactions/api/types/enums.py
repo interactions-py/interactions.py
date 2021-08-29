4831695db3f47958d493eb7c59755e1f139d854e
DeltaXWizard <33706469+DeltaXWizard@users.noreply.github.com>
2021-08-29 00:23:52 -0400

    Add Guild-Features enum, switch datetime type, add most of Guild object

--- interactions/api/types/enums.py
+++ interactions/api/types/enums.py
@@ -1,5 +1,5 @@
 import typing
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 from discord import Member
 from discord.abc import Messageable, User, Role, GuildChannel
@@ -89,6 +89,36 @@ class HTTPResponse(IntEnum):
     METHOD_NOT_ALLOWED = 405
     TOO_MANY_REQUESTS = 429
     GATEWAY_UNAVAILABLE = 502
+
+
+class GuildFeature(str, Enum):
+    """Lists all of the features in any Guild in a string Enum.
+
+    ..note:
+        Equivalent of `Guild Features` <https://discord.com/developers/docs/resources/guild#guild-object-guild-features>
+    """
+
+    ANIMATED_ICON = "ANIMATED_ICON"
+    BANNER = "BANNER"
+    COMMERCE = "COMMERCE"
+    COMMUNITY = "COMMUNITY"
+    DISCOVERABLE = "DISCOVERABLE"
+    FEATURABLE = "FEATURABLE"
+    INVITE_SPLASH = "INVITE_SPLASH"
+    MEMBER_VERIFICATION_GATE_ENABLED = "MEMBER_VERIFICATION_GATE_ENABLED"
+    NEWS = "NEWS"
+    PARTNERED = "PARTNERED"
+    PREVIEW_ENABLED = "PREVIEW_ENABLED"
+    VANITY_URL = "VANITY_URL"
+    VERIFIED = "VERIFIED"
+    VIP_REGIONS = "VIP_REGIONS"
+    WELCOME_SCREEN_ENABLED = "WELCOME_SCREEN_ENABLED"
+    TICKETED_EVENTS_ENABLED = "TICKETED_EVENTS_ENABLED"
+    MONETIZATION_ENABLED = "MONETIZATION_ENABLED"
+    MORE_STICKERS = "MORE_STICKERS"
+    THREE_DAY_THREAD_ARCHIVE = "THREE_DAY_THREAD_ARCHIVE"
+    SEVEN_DAY_THREAD_ARCHIVE = "SEVEN_DAY_THREAD_ARCHIVE"
+    PRIVATE_THREADS = "PRIVATE_THREADS"
 
 
 class Options(IntEnum):
--- interactions/api/types/models.py
+++ interactions/api/types/models.py
@@ -5,6 +5,9 @@ from typing import Optional, Union, List
 # TODO: Reorganise these models based on which big obj uses little obj
 # TODO: Figure out ? placements.
 
+# 'Optional[datetime.datetime]` is the Timestamp, just a mini note
+from interactions.api.types.enums import GuildFeature
+
 
 class MessageActivity(object):
     type: int
@@ -61,7 +64,7 @@ class Member(object):
     nick: Optional[str]
     roles: List[str]
     joined_at: datetime.datetime.timestamp
-    premium_since: Optional[datetime.datetime.timestamp]
+    premium_since: Optional[datetime.datetime]
     deaf: bool
     mute: bool
     pending: Optional[bool]
@@ -102,7 +105,7 @@ class Channel(object):
     owner_id: Optional[int]
     application_id: Optional[int]
     parent_id: Optional[int]
-    last_pin_timestamp: Optional[datetime.datetime.timestamp]
+    last_pin_timestamp: Optional[datetime.datetime]
     rtc_region: Optional[str]
     video_quality_mode: Optional[int]
     message_count: Optional[int]
@@ -141,7 +144,7 @@ class Message(object):
     member: Optional[Member]
     content: str
     timestamp: datetime.datetime.timestamp
-    edited_timestamp: Optional[datetime.datetime.timestamp]
+    edited_timestamp: Optional[datetime.datetime]
     tts: bool
     mention_everyone: bool
     # mentions: array of Users, and maybe partial members
@@ -221,3 +224,87 @@ class ChannelMention(object):
     guild_id: int
     type: int  # Replace with enum from Channel Type, another PR
     name: str
+
+class PresenceActivity(object):
+    name: str
+    type: int
+    url: Optional[str]
+    created_at: int
+    timestamps: Optional[datetime.datetime]
+    application_id: Optional[int]
+    details: Optional[str]
+    state: Optional[str]
+    emoji: Optional[Emoji]
+    # party
+    #assets
+    # secrets
+    instance: Optional[bool]
+    flags: Optional[int]
+    # buttons
+
+class ClientStatus(object):
+    desktop: Optional[str]
+    mobile: Optional[str]
+    web: Optional[str]
+
+class PresenceUpdate(object):
+    user: User
+    guild_id: int
+    status: str
+    activities: List[PresenceActivity]
+    client_status: ClientStatus
+
+
+class Guild(object):
+    """The big Guild object."""
+
+    id: int
+    name: str
+    icon: Optional[str]
+    icon_hash: Optional[str]
+    splash: Optional[str]
+    discovery_splash: Optional[str]
+    owner: Optional[bool]
+    owner_id: int
+    permissions: Optional[str]
+    region: Optional[str]  # None, we don't do Voices.
+    afk_channel_id: Optional[int]
+    afk_timeout: int
+    widget_enabled: Optional[bool]
+    widget_channel_id: Optional[int]
+    verification_level: int
+    default_message_notifications: int
+    explicit_content_filter: int
+    roles: List[Role]
+    emojis: List[Emoji]
+    features: List[GuildFeature]
+    mfa_level: int
+    application_id: Optional[int]
+    system_channel_id: Optional[int]
+    system_channel_flags: int
+    rules_channel_id: Optional[int]
+    joined_at: Optional[datetime.datetime]
+    large: Optional[bool]
+    unavailable: Optional[bool]
+    member_count: Optional[int]
+    # Voice states. They're None due to v4.0 property
+    members: Optional[List[Member]]
+    channels: Optional[List[Channel]]
+    threads: Optional[List[Channel]]  # threads, because of their metadata
+    # presences: Optional[List[]]
+    max_presences: Optional[int]
+    max_members: Optional[int]
+    vanity_url_code: Optional[str]
+    description: Optional[str]
+    banner: Optional[str]
+    premium_tier: int
+    premium_subscription_count: Optional[int]
+    preferred_locale: str
+    public_updates_channel_id: Optional[int]
+    max_video_channel_users: Optional[int]
+    approximate_member_count: Optional[int]
+    approximate_presence_count: Optional[int]
+    # welcome_Screen
+    nsfw_level: int
+    # stage_instances
+    # stickers
