b670003e2ef860b834218782113929034ff1d545
Sirius <41456914+Sirius3970@users.noreply.github.com>
2021-10-04 13:06:09 -0400

    Testing new RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ version = ".".join(__version__.split(".", 2)[:2])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "karma_sphinx_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "insipid"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -56,12 +56,13 @@ autodoc_member_order = "bysource"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "karma_sphinx_theme"
+html_theme = "insipid"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["colors.css"]
 
 # Intersphinx
 intersphinx_mapping = {
