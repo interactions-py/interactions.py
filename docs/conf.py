84cbd46e0383eee95a2dbcb80dba1e69336a4a78
Sirius <41456914+Sirius3970@users.noreply.github.com>
2021-10-05 00:57:55 -0400

    maybe backwards?

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ version = ".".join(__version__.split(".", 2)[:2])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_book_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "book_sphinx_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -56,7 +56,7 @@ autodoc_member_order = "bysource"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "book_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
