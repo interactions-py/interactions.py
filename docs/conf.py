6a8fcda06a265f86a5b1098a18fb9c710a572bd9
Sirius <41456914+Sirius3970@users.noreply.github.com>
2021-10-05 00:55:24 -0400

    new theme pls?

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ version = ".".join(__version__.split(".", 2)[:2])
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_book_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
