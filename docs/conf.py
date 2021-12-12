3e154fc556e7e4cd45ee80480f3420696feff8be
James Walston <officialdeltadev@gmail.com>
2021-12-12 01:39:09 -0500

    chore: work on internationalization for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -57,8 +57,9 @@ templates_path = ["_templates"]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 locale_dirs = ["locale/"]
+gettext_compact = True
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
