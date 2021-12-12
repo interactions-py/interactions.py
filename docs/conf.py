ab913e65b64765ac7794acdead0504426479ee09
James Walston <officialdeltadev@gmail.com>
2021-12-12 01:55:48 -0500

    feat: test building locale doc

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ templates_path = ["_templates"]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "en"
+language = "de"
 locale_dirs = ["locale/"]
 gettext_compact = True
 
