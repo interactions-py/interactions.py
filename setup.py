89767731b5a9e518213659e15d0ebe006708aeb4
DeltaXWizard <33706469+deltaxwizard@users.noreply.github.com>
2021-12-31 22:08:30 -0500

    fix: Add discord.py setup check by RTD standards.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author_email="jwalston2002@gmail.com",
     description="A slash command handler for discord.py.",
     extras_require=extras,
-    install_requires=["discord.py", "aiohttp"],
+    install_requires=["discord.py>=1.0.0", "aiohttp"],
     license="MIT License",
     long_description=README,
     long_description_content_type="text/markdown",
