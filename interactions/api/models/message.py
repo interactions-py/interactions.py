be25884ffb09b4f5936fd42f985bf527ec8c9137
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-01-19 16:50:48 +0100

    docs: - fix message.py

--- interactions/api/models/message.py
+++ interactions/api/models/message.py
@@ -282,7 +282,8 @@ class Message(DictSerializerMixin):
 
     async def get_channel(self) -> Channel:
         """
-        Gets the channel where the message was sent
+        Gets the channel where the message was sent.
+
         :rtype: Channel
         """
         res = await self._client.get_channel(channel_id=int(self.channel_id))
@@ -290,7 +291,8 @@ class Message(DictSerializerMixin):
 
     async def get_guild(self):
         """
-        Gets the guild where the message was sent
+        Gets the guild where the message was sent.
+
         :rtype: Guild
         """
         from .guild import Guild
@@ -301,6 +303,7 @@ class Message(DictSerializerMixin):
     async def delete(self, reason: Optional[str] = None) -> None:
         """
         Deletes the message.
+
         :param reason: Optional reason to show up in the audit log. Defaults to `None`.
         :type reason: Optional[str]
         """
