ed599463ab4a822a8df674af236e7c52e299fb30
Meido no Hitsuji <popenkodaniil@mail.ru>
2022-01-20 09:36:37 +0700

    fix(gateway,component): SelectMenu parse per SelectOption
    * (f) fix SelectMenu and addition add values in message component
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * (f) fix check_sub_auto
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- interactions/api/gateway.py
+++ interactions/api/gateway.py
@@ -261,7 +261,7 @@ class WebSocket:
                 if option["type"] == OptionType.SUB_COMMAND_GROUP:
                     for group_option in option["options"]:
                         if group_option.get("options"):
-                            for sub_option in option["options"]:
+                            for sub_option in group_option["options"]:
                                 if sub_option.get("focused"):
                                     return sub_option["name"], sub_option["value"]
                 elif option["type"] == OptionType.SUB_COMMAND:
@@ -314,6 +314,8 @@ class WebSocket:
                                     )
                     elif data["type"] == InteractionType.MESSAGE_COMPONENT:
                         _name = f"component_{context.data.custom_id}"
+                        if context.data._json.get("values"):
+                            _args.append(context.data.values)
                     elif data["type"] == InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE:
                         _name = f"autocomplete_{context.data.id}"
                         if context.data._json.get("options"):
