7900a0e3085e07a91ac13571e96007981c847a5b
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-02-14 21:41:49 +0100

    revert: correct merge conflicts

--- interactions/api/models/flags.py
+++ interactions/api/models/flags.py
@@ -121,9 +121,15 @@ class AppFlags(IntFlag):
     GATEWAY_MESSAGE_CONTENT_LIMITED = 1 << 19
 
 
+<<<<<<< HEAD
 class StatusType(str, Enum):
     """
     An enumerable object representing Discord status icons that a user may have.
+=======
+class StatusType(StrEnum):
+    """
+    A string enum representing Discord status icons that a user may have.
+>>>>>>> e1320ac (feat: Add localisation enum (#522))
     """
 
     ONLINE = "online"
--- interactions/enums.py
+++ interactions/enums.py
@@ -1,4 +1,4 @@
-from enum import Enum, IntEnum
+from enum import IntEnum, StrEnum
 
 
 class ApplicationCommandType(IntEnum):
