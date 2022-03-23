196c3657d1bd42ccc9094832d4436ceddeb8f0ec
fl0w <41456914+goverfl0w@users.noreply.github.com>
2022-03-23 12:31:32 -0400

    style: log missing attrs as `INFO`.
    This was an unanimous decision that was made by both the core and external development teams. For reasons involving how Discord staff view matters of silently releasing new features to the library, we've made the decision to no longer treat these as superficial problems that break the runtime of a client running on the library. Since our library continues to operate normally with a missing attribute or data model, there is no need to log as such.

--- interactions/api/models/misc.py
+++ interactions/api/models/misc.py
@@ -41,7 +41,7 @@ class DictSerializerMixin(object):
                 # else case if the mixin is used outside of this library and/or SDK.
                 setattr(self, key, kwargs[key])
             else:
-                log.warning(
+                log.info(
                     f"Attribute {key} is missing from the {self.__class__.__name__} data model, skipping."
                 )
                 # work on message printout? Effective, but I think it should be a little bit more friendly
