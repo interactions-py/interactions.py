9e05bc19dcf91299e8bdb0a2789797d0b6d08fbc
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-03-31 21:25:25 +0200

    feat: ``client.get()``
    - refactor code

--- interactions/client/bot.py
+++ interactions/client/bot.py
@@ -1066,8 +1066,8 @@ class Client:
             When getting an object you must pass its required attributes. Those are listed below:
                 * Channel: channel_id
                 * Guild: guild_id
-                * Message: channel_id and message_id
                 * Member: guild_id and user_id
+                * Message: channel_id and message_id
 
         .. code-block:: python
             ...
@@ -1104,6 +1104,16 @@ class Client:
             guild._client = self._http
             return guild
 
+        elif isinstance(obj, Member):
+            if not guild_id or not user_id:
+                raise AttributeError("Please specify guild_id and user_id for getting a member!")
+
+            # todo: cache impl
+
+            return Member(
+                **await self._http.get_member(int(guild_id), int(user_id)), _client=self._http
+            )
+
         elif isinstance(obj, Message):
             if not channel_id or not message_id:
                 raise AttributeError(
@@ -1121,16 +1131,6 @@ class Client:
                 return message
 
             return Message(**message, _client=self._http)
-
-        elif isinstance(obj, Member):
-            if not guild_id or not user_id:
-                raise AttributeError("Please specify guild_id and user_id for getting a member!")
-
-            # todo: cache impl
-
-            return Member(
-                **await self._http.get_member(int(guild_id), int(user_id)), _client=self._http
-            )
 
     def get_extension(self, name: str) -> Optional[Union[ModuleType, "Extension"]]:
         return self._extensions.get(name)
