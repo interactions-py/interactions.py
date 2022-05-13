5b23ecfdae49f952d063755cef22c67b6b79c5d0
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-05-13 20:10:47 +0200

    ci

--- interactions/client/bot.py
+++ interactions/client/bot.py
@@ -1427,8 +1427,8 @@ class Client:
 
         :param obj: The object to get. Should be a class object (not an instance!). For example: `interactions.Channel`.
         :type obj: object
-        :param \**kwargs: The arguments to pass to the HTTP method.
-        :type \**kwargs: dict
+        :param **kwargs: The arguments to pass to the HTTP method.
+        :type **kwargs: dict
         :return: The object we're trying to get.
         :rtype: object
         """
