c02933f22ef246cf1ad33386b255429a604351e9
Nan R <nanbusiness13@gmail.com>
2022-05-26 21:56:58 +0200

    Revert "Update slotting"
    This reverts commit 66b05100bf6f241b1f35a189fe5b63ff4e825a88.

--- interactions/api/http/invite.py
+++ interactions/api/http/invite.py
@@ -30,6 +30,7 @@ class InviteRequest:
             f"/invites/{invite_code}{'?' + final if final is not None else ''}"
         ))
 
+
     async def delete_invite(self, invite_code: str) -> Invite:
         """
         Deletes an invite.
