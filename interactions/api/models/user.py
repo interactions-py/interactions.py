5a332b651f4abb4e8bd39fb5b1cb67f78d72d035
Nan R <nanbusiness13@gmail.com>
2022-05-26 09:32:49 +0200

    feat: Add has_public_flag to User model

--- interactions/api/models/user.py
+++ interactions/api/models/user.py
@@ -65,6 +65,11 @@ class User(DictSerializerMixin):
     def __repr__(self) -> str:
         return self.username
 
+    def has_public_flag(self, flag: UserFlags) -> bool:
+        if self.public_flags == 0 or self.public_flags is None:
+            return False
+        return (int(self.public_flags) & int(flag)) == int(flag)
+
     @property
     def mention(self) -> str:
         """
