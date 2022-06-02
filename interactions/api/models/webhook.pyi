536c4c9c8fb9ad692d348a5e0f241429ce819da7
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-06-01 22:34:46 -0400

    fix: convert Webhook to the newer dict mixin

--- interactions/api/models/webhook.pyi
+++ interactions/api/models/webhook.pyi
@@ -1,22 +1,24 @@
 from enum import IntEnum
-from .misc import DictSerializerMixin, Snowflake, Image, MISSING, File
+from typing import Any, List, Optional, Union
+
+from .attrs_utils import ClientSerializerMixin, MISSING, define
 from .channel import Channel
 from .guild import Guild
-from typing import Union, Optional, Any, List
+from .message import Embed, Message
+from .misc import File, Image, Snowflake
 from .user import User
 from ..http.client import HTTPClient
-from ...client.models.component import Button, SelectMenu, ActionRow
-from .message import Message, Embed
+from ...client.models.component import ActionRow, Button, SelectMenu
+
 
 class WebhookType(IntEnum):
     Incoming: int
     Channel_Follower: int
     Application: int
 
-class Webhook(DictSerializerMixin):
 
-    _json: dict
-    _client: HTTPClient
+@define()
+class Webhook(ClientSerializerMixin):
     id: Snowflake
     type: Union[WebhookType, int]
     guild_id: Optional[Snowflake]
@@ -30,10 +32,11 @@ class Webhook(DictSerializerMixin):
     source_channel: Optional[Channel]
     url: Optional[str]
 
-    def __init__(self, **kwargs): ...
+    def __attrs_post_init__(self): ...
+
     @classmethod
     async def create(
-        cls, client: HTTPClient, channel_id: int, name: str, avatar: Optional[Image] = MISSING
+            cls, client: HTTPClient, channel_id: int, name: str, avatar: Optional[Image] = MISSING
     ) -> "Webhook": ...
     @classmethod
     async def get(cls, client: HTTPClient, webhook_id: int, webhook_token: Optional[str] = MISSING) -> "Webhook": ...
