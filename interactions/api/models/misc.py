a51e7c2dd3248fb5c0a33a16f8cee995f5b76414
GeomKid <huntiktan188@gmail.com>
2022-06-06 20:29:52 +0800

    Update misc.py

--- interactions/api/models/misc.py
+++ interactions/api/models/misc.py
@@ -12,6 +12,8 @@ from logging import Logger
 from math import floor
 from os.path import basename
 from typing import Optional, Union
+import colorsys
+import random
 
 from interactions.api.models.attrs_utils import MISSING, DictSerializerMixin, define, field
 from interactions.base import get_logger
@@ -159,53 +161,222 @@ class Snowflake(object):
 
 class Color(object):
     """
-    An object representing Discord branding colors.
-
-    .. note::
-        This object only intends to cover the branding colors
-        and no others. The main reason behind this is due to
-        the current accepted standard of using hex codes or other
-        custom-defined colors.
+    An object representing colors.
     """
+    __slots__ = ("value",)
+
+    def __init__(self, value):
+        if not isinstance(value, int):
+            raise TypeError(
+                "Expected int parameter, received %s instead." % value.__class__.__name__
+            )
+
+        self.value = value
+
+    def _get_byte(self, byte):
+        return (self.value >> (8 * byte)) & 0xFF
+
+    def __eq__(self, other):
+        if not isinstance(other, Color):
+            return NotImplementedError()
+        return self.value == other.value
+
+    def __str__(self):
+        return "#{:0>6x}".format(self.value)
+
+    def __repr__(self):
+        return "<Colour value=%s>" % self.value
+
+    def __hash__(self):
+        return hash(self.value)
 
     @property
-    def blurple(self) -> hex:
-        """Returns a hexadecimal value of the blurple color."""
-        return 0x5865F2
+    def r(self):
+        """:class:`int`: Returns the red component of the colour."""
+        return self._get_byte(2)
 
     @property
-    def green(self) -> hex:
-        """Returns a hexadecimal value of the green color."""
-        return 0x57F287
+    def g(self):
+        """:class:`int`: Returns the green component of the colour."""
+        return self._get_byte(1)
 
     @property
-    def yellow(self) -> hex:
-        """Returns a hexadecimal value of the yellow color."""
-        return 0xFEE75C
+    def b(self):
+        """:class:`int`: Returns the blue component of the colour."""
+        return self._get_byte(0)
 
-    @property
-    def fuchsia(self) -> hex:
+    def to_rgb(self):
+        """Tuple[:class:`int`, :class:`int`, :class:`int`]: Returns an (r, g, b) tuple representing the colour."""
+        return (self.r, self.g, self.b)
+
+    @classmethod
+    def from_rgb(cls, r, g, b):
+        """Constructs a :class:`Colour` from an RGB tuple."""
+        return cls((r << 16) + (g << 8) + b)
+
+    @classmethod
+    def from_hsv(cls, h, s, v):
+        """Constructs a :class:`Colour` from an HSV tuple."""
+        rgb = colorsys.hsv_to_rgb(h, s, v)
+        return cls.from_rgb(*(int(x * 255) for x in rgb))
+
+    @classmethod
+    def default(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0``."""
+        return cls(0)
+
+    @classmethod
+    def random(cls, *, seed=None):
+        """A class method that returns a :class:`Colour` with a random hue.
+
+        .. note::
+
+            The random algorithm works by choosing a colour with a random hue but
+            with maxed out saturation and value.
+        Parameters
+        ------------
+        seed: Optional[Union[:class:`int`, :class:`str`, :class:`float`, :class:`bytes`, :class:`bytearray`]]
+            The seed to initialize the RNG with. If ``None`` is passed the default RNG is used.
+
+        """
+        rand = random if seed is None else random.Random(seed)
+        return cls.from_hsv(rand.random(), 1, 1)
+
+    @classmethod
+    def teal(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x1abc9c``."""
+        return cls(0x1ABC9C)
+
+    @classmethod
+    def dark_teal(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x11806a``."""
+        return cls(0x11806A)
+
+    @classmethod
+    def green(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x2ecc71``."""
+        return cls(0x2ECC71)
+
+    @classmethod
+    def dark_green(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x1f8b4c``."""
+        return cls(0x1F8B4C)
+
+    @classmethod
+    def blue(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x3498db``."""
+        return cls(0x3498DB)
+
+    @classmethod
+    def dark_blue(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x206694``."""
+        return cls(0x206694)
+
+    @classmethod
+    def purple(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x9b59b6``."""
+        return cls(0x9B59B6)
+
+    @classmethod
+    def dark_purple(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x71368a``."""
+        return cls(0x71368A)
+
+    @classmethod
+    def magenta(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xe91e63``."""
+        return cls(0xE91E63)
+
+    @classmethod
+    def dark_magenta(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xad1457``."""
+        return cls(0xAD1457)
+
+    @classmethod
+    def gold(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xf1c40f``."""
+        return cls(0xF1C40F)
+
+    @classmethod
+    def dark_gold(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xc27c0e``."""
+        return cls(0xC27C0E)
+
+    @classmethod
+    def orange(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xe67e22``."""
+        return cls(0xE67E22)
+
+    @classmethod
+    def dark_orange(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xa84300``."""
+        return cls(0xA84300)
+
+    @classmethod
+    def red(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0xed4245``."""
+        return cls(0xED4245)
+
+    @classmethod
+    def dark_red(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x992d22``."""
+        return cls(0x992D22)
+
+    @classmethod
+    def lighter_grey(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x95a5a6``."""
+        return cls(0x95A5A6)
+
+    @classmethod
+    def dark_grey(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x607d8b``."""
+        return cls(0x607D8B)
+
+    @classmethod
+    def light_grey(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x979c9f``."""
+        return cls(0x979C9F)
+
+    @classmethod
+    def darker_grey(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x546e7a``."""
+        return cls(0x546E7A)
+
+    @classmethod
+    def blurple(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x5865f2``."""
+        return cls(0x5865F2)
+
+    @classmethod
+    def greyple(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x99aab5``."""
+        return cls(0x99AAB5)
+
+    @classmethod
+    def dark_theme(cls):
+        """A class method that returns a :class:`Colour` with a value of ``0x36393F``.
+        This will appear transparent on Discord's dark theme.
+        """
+        return cls(0x36393F)
+
+    @classmethod
+    def fuchsia(cls):
         """Returns a hexadecimal value of the fuchsia color."""
-        return 0xEB459E
-
-    @property
-    def red(self) -> hex:
-        """Returns a hexadecimal value of the red color."""
-        return 0xED4245
+        return cls(0xEB459E)
 
     # I can't imagine any bot developers actually using these.
     # If they don't know white is ff and black is 00, something's seriously
     # wrong.
 
-    @property
-    def white(self) -> hex:
+    @classmethod
+    def white(cls):
         """Returns a hexadecimal value of the white color."""
-        return 0xFFFFFF
+        return cls(0xFFFFFF)
 
-    @property
-    def black(self) -> hex:
+    @classmethod
+    def black(cls):
         """Returns a hexadecimal value of the black color."""
-        return 0x000000
+        return cls(0x000000)
 
 
 class File(object):
@@ -225,7 +396,8 @@ class File(object):
 
         if not isinstance(filename, str):
             raise TypeError(
-                "File's first parameter 'filename' must be a string, not " + str(type(filename))
+                "File's first parameter 'filename' must be a string, not " +
+                str(type(filename))
             )
 
         self._fp = open(filename, "rb") if not fp or fp is MISSING else fp
