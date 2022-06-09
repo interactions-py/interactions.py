092f23b4dec6ed30eb83cbc5c5809e99dffa4257
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2022-06-09 11:47:02 -0400

    fix: parse cub cmd (and groups) options properly

--- interactions/client/models/command.py
+++ interactions/client/models/command.py
@@ -114,7 +114,12 @@ class Option(DictSerializerMixin):
     def __attrs_post_init__(self):
         # needed for nested classes
         self.options = (
-            [Option(**option) for option in self.options] if self.options is not None else None
+            [
+                Option(**option._json) if hasattr(option, "_json") else Option(**option)  # type: ignore
+                for option in self.options
+            ]
+            if self.options is not None
+            else None
         )
 
 
