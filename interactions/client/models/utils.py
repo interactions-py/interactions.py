dc693bd1d35eb9ed5f3f2faa50e19b75320f9388
Toricane <prjwl028@gmail.com>
2022-07-08 20:42:00 -0700

    docs: utils.py docstrings

--- interactions/client/models/utils.py
+++ interactions/client/models/utils.py
@@ -1,6 +1,6 @@
 from asyncio import Task, get_running_loop, sleep
 from functools import wraps
-from typing import TYPE_CHECKING, Any, Callable, List, Optional, Union
+from typing import TYPE_CHECKING, Awaitable, Callable, List, Union
 
 from ...api.error import LibraryException
 from .component import ActionRow, Button, SelectMenu
@@ -13,24 +13,43 @@ __all__ = ("autodefer", "spread_to_rows")
 
 
 def autodefer(
-    delay: Optional[Union[float, int]] = 2,
-    ephemeral: Optional[bool] = False,
-    edit_origin: Optional[bool] = False,
-):  # TODO: test this out
+    delay: Union[float, int] = 2,
+    ephemeral: bool = False,
+    edit_origin: bool = False,
+) -> Callable[[Callable[..., Awaitable]], Callable[..., Awaitable]]:
     """
-    docstring
-    """  # TODO: change docstring
+    A decorator that automatically defers a command if it did not respond within ``delay`` seconds.
 
-    def inner(func: Callable[..., Any]) -> Callable[..., Any]:
+    The structure of the decorator is:
+
+    .. code-block:: python
+
+        @bot.command()
+        @autodefer()  # configurable
+        async def command(ctx):
+            await asyncio.sleep(5)
+            await ctx.send("I'm awake now!")
+
+    :param delay?: The amount of time in seconds to wait before defering the command. Defaults to ``2`` seconds.
+    :type delay: Union[float, int]
+    :param ephemeral?: Whether the command is deferred ephemerally. Defaults to ``False``.
+    :type ephemeral: bool
+    :param edit_origin?: Whether the command is deferred on origin. Defaults to ``False``.
+    :type edit_origin: bool
+    :return: The inner function, for decorating.
+    :rtype:
+    """
+
+    def decorator(coro: Callable[..., Awaitable]) -> Callable[..., Awaitable]:
         from ..context import ComponentContext
 
-        @wraps(func)
+        @wraps(coro)
         async def deferring_func(ctx: Union["CommandContext", "ComponentContext"], *args, **kwargs):
             try:
                 loop = get_running_loop()
             except RuntimeError as e:
                 raise RuntimeError("No running event loop detected!") from e
-            task: Task = loop.create_task(func(ctx, *args, **kwargs))
+            task: Task = loop.create_task(coro(ctx, *args, **kwargs))
 
             await sleep(delay)
 
@@ -47,15 +66,43 @@ def autodefer(
 
         return deferring_func
 
-    return inner
+    return decorator
 
 
 def spread_to_rows(
     *components: Union[ActionRow, Button, SelectMenu], max_in_row: int = 5
-) -> List[ActionRow]:  # TODO: test this out
+) -> List[ActionRow]:
+    r"""
+    A helper function that spreads components into ``ActionRow``s.
+
+    Example:
+
+    .. code-block:: python
+
+        @bot.command()
+        async def command(ctx):
+            b1 = Button(style=1, custom_id="b1", label="b1")
+            b2 = Button(style=1, custom_id="b2", label="b2")
+            s1 = SelectMenu(
+                custom_id="s1",
+                options=[
+                    SelectOption(label="1", value="1"),
+                    SelectOption(label="2", value="2"),
+                ],
+            )
+            b3 = Button(style=1, custom_id="b3", label="b3")
+            b4 = Button(style=1, custom_id="b4", label="b4")
+
+            await ctx.send("Components:", components=spread_to_rows(b1, b2, s1, b3, b4))
+
+    .. note::
+        You can only pass in ``ActionRow``s, ``Button``s, and ``SelectMenu``s, but in any order.
+
+    :param \*components: The components to spread.
+    :type \*components: Union[ActionRow, Button, SelectMenu]
+    :param max_in_row?: The maximum number of components in a single row. Defaults to ``5``.
+    :type max_in_row: int
     """
-    docstring
-    """  # TODO: change docstring
     if not components or len(components) > 25:
         raise LibraryException(code=12, message="Number of components should be between 1 and 25.")
     if not 1 <= max_in_row <= 5:
