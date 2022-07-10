6a7dce07bdefb68882cc72e4468fa52789478c65
Toricane <prjwl028@gmail.com>
2022-07-09 20:31:25 -0700

    docs: fix incorrect typehint

--- interactions/client/models/component.py
+++ interactions/client/models/component.py
@@ -1,5 +1,5 @@
 import contextlib
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 from ...api.error import LibraryException
 from ...api.models.attrs_utils import MISSING, DictSerializerMixin, convert_list, define, field
@@ -297,12 +297,12 @@ class ActionRow(ComponentMixin):
             self._json["components"] = [component._json for component in self.components]
 
     @classmethod
-    def new(cls, *components: Tuple[Component]) -> "ActionRow":
+    def new(cls, *components: Component) -> "ActionRow":
         r"""
         A class method for creating a new ``ActionRow``.
 
         :param \*components: The components to add to the ``ActionRow``.
-        :type \*components: Tuple[Component]
+        :type \*components: Component
         :return: A new ``ActionRow``.
         :rtype: ActionRow
         """
