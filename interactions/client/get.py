3c78c4d66adcab52119d0f209bf72cfc10b1c4ae
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-07-11 07:33:04 +0200

    Update interactions/client/get.py
    Co-authored-by: Sofia <41456914+ffl0w@users.noreply.github.com>

--- interactions/client/get.py
+++ interactions/client/get.py
@@ -36,7 +36,7 @@ __all__ = (
 
 class Force(str, Enum):
     """
-    An enum representing the force types for the get method.
+    An enumerable object representing the force types for the get method.
 
     :ivar str CACHE: Enforce the usage of cache and block the usage of http
     :ivar str HTTP: Enforce the usage of http and block the usage of cache
