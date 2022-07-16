b330ec1669d3d09f0d960d00681ba7d248f01485
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-07-16 17:46:44 +0200

    Update interactions/client/models/command.py
    Co-authored-by: Toricane <73972068+Toricane@users.noreply.github.com>

--- interactions/client/models/command.py
+++ interactions/client/models/command.py
@@ -106,7 +106,7 @@ class Option(DictSerializerMixin):
     :ivar Optional[bool] autocomplete?: A status denoting whether this option is an autocomplete option.
     :ivar Optional[Dict[Union[str, Locale], str]] name_localizations?: The dictionary of localization for the ``name`` field. This enforces the same restrictions as the ``name`` field.
     :ivar Optional[Dict[Union[str, Locale], str]] description_localizations?: The dictionary of localization for the ``description`` field. This enforces the same restrictions as the ``description`` field.
-    :ivar Optional[str] converter: How the argument for the option in your function is called, if any
+    :ivar Optional[str] converter: How the argument for the option in your function is passed, if any
     """
 
     type: OptionType = field(converter=OptionType)
