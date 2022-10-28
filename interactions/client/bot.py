e95ebfdbedaa69f6ec6aafb98814afca98747012
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-10-28 11:30:36 -0400

    Update interactions/client/bot.py
    Co-authored-by: Toricane <73972068+Toricane@users.noreply.github.com>

--- interactions/client/bot.py
+++ interactions/client/bot.py
@@ -80,7 +80,7 @@ class Client:
         cache_limits: Optional[Dict[type, int]] = None,
         intents: Intents = Intents.DEFAULT,
         shards: Optional[List[Tuple[int]]] = None,
-        default_scope: Union[int, Snowflake, List[int, Snowflake]] = None,
+        default_scope: Union[int, Snowflake, List[Union[int, Snowflake]]] = None,
         presence: Optional[ClientPresence] = None,
         _logging: Union[bool, int] = None,
         disable_sync: bool = False,
