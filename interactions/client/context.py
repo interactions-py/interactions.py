0f0b6a0aec16eb0efc8721f41404a8e6cdb17fda
Damego <damego.dev@gmail.com>
2022-11-22 20:58:56 +0500

    docs: Add missed docstring to context; Hide _Context (#1176)
    * docs: Add missed docstring to context; Hide _Context
    
    * ci: correct from checks.
    
    * what?
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- interactions/client/context.py
+++ interactions/client/context.py
@@ -38,12 +38,6 @@ class _Context(ClientSerializerMixin):
     that the user-facing API is able to allow developers to
     easily access information presented from any event in
     a "contextualized" sense.
-
-    :ivar Optional[Message] message: The message data model.
-    :ivar Member author: The member data model.
-    :ivar User user: The user data model.
-    :ivar Optional[Channel] channel: The channel data model.
-    :ivar Optional[Guild] guild: The guild data model.
     """
 
     message: Optional[Message] = field(converter=Message, default=None, add_client=True)
@@ -231,9 +225,16 @@ class _Context(ClientSerializerMixin):
     ) -> dict:  # sourcery skip: low-code-quality
         """
         This allows the invocation state described in the "context"
-        to send an interaction response. This inherits the arguments
-        of the :func:`_Context.send` method.
+        to send an interaction response.
 
+        :param Optional[str] content: The contents of the message as a string or string-converted value.
+        :param Optional[bool] tts: Whether the message utilizes the text-to-speech Discord programme or not.
+        :param Optional[List[Attachment]] attachments: The attachments to attach to the message. Needs to be uploaded to the CDN first
+        :param Optional[Union[File, List[File]]] files: The files to attach to the message.
+        :param Optional[Union[Embed, List[Embed]]] embeds: An embed, or list of embeds for the message.
+        :param Optional[Union[AllowedMentions, dict]] allowed_mentions: The allowed mentions for the message.
+        :param Optional[MessageReference] message_reference: Include to make your message a reply.
+        :param Optional[Union[ActionRow, Button, SelectMenu, List[Union[ActionRow, Button, SelectMenu]]]] components: A component, or list of components for the message.
         :return: The edited message as a dict.
         :rtype: dict
         """
@@ -353,7 +354,7 @@ class _Context(ClientSerializerMixin):
 @define()
 class CommandContext(_Context):
     """
-    A derivation of :class:`_Context`
+    A derivation of context
     designed specifically for application command data.
 
     :ivar Snowflake id: The ID of the interaction.
@@ -364,6 +365,10 @@ class CommandContext(_Context):
     :ivar str token: The token of the interaction response.
     :ivar Snowflake guild_id: The ID of the current guild.
     :ivar Snowflake channel_id: The ID of the current channel.
+    :ivar Member author: The member data model.
+    :ivar User user: The user data model.
+    :ivar Optional[Channel] channel: The channel data model.
+    :ivar Optional[Guild] guild: The guild data model.
     :ivar bool responded: Whether an original response was made or not.
     :ivar bool deferred: Whether the response was deferred or not.
     :ivar Optional[Locale] locale: The selected language of the user invoking the interaction.
@@ -600,12 +605,29 @@ class CommandContext(_Context):
 @define()
 class ComponentContext(_Context):
     """
-    A derivation of :class:`_Context`
+    A derivation of context
     designed specifically for component data.
+
+    :ivar Snowflake id: The ID of the interaction.
+    :ivar Snowflake application_id: The application ID of the interaction.
+    :ivar InteractionType type: The type of interaction.
+    :ivar InteractionData data: The application command data.
+    :ivar str token: The token of the interaction response.
+    :ivar Snowflake guild_id: The ID of the current guild.
+    :ivar Snowflake channel_id: The ID of the current channel.
+    :ivar Optional[Message] message: The message data model.
+    :ivar Member author: The member data model.
+    :ivar User user: The user data model.
+    :ivar Optional[Channel] channel: The channel data model.
+    :ivar Optional[Guild] guild: The guild data model.
+    :ivar bool responded: Whether an original response was made or not.
+    :ivar bool deferred: Whether the response was deferred or not.
+    :ivar str locale: The selected language of the user invoking the interaction.
+    :ivar str guild_locale: The guild's preferred language, if invoked in a guild.
+    :ivar str app_permissions: Bitwise set of permissions the bot has within the channel the interaction was sent from.
     """
 
     async def edit(self, content: Optional[str] = MISSING, **kwargs) -> Message:
-
         payload, files = await super().edit(content, **kwargs)
         msg = None
 
