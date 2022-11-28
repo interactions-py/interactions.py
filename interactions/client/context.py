34a1da726fd1d88500bbe5efbcfb0b211954bc48
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-11-28 09:39:53 +0100

    Update interactions/client/context.py

--- interactions/client/context.py
+++ interactions/client/context.py
@@ -115,6 +115,7 @@ class _Context(ClientSerializerMixin):
     @property
     def guild(self) -> Optional[Guild]:
         """
+        .. versionadded:: 4.1.0
          .. versionchanged:: 4.4.0
             Guild now returns ``None`` instead of ``MISSING`` if it is not found to avoid confusion
 
