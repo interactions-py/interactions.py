98039179f55e0fa9edbfd5628e3ae36edffd7ec9
GeomKid <51281740+GeomKid@users.noreply.github.com>
2023-01-17 23:45:24 +0800

    fix: dying due to error

--- interactions/client/bot.py
+++ interactions/client/bot.py
@@ -696,7 +696,7 @@ class Client:
                             raise LibraryException(50001, message="Missing Access |")
                         if _guild_id not in _guild_ids:
                             log.warning(f"The bot is not in guild with id {_guild_id}")
-                            raise LibraryException(50001, message="Missing Access |")
+                            continue
                         if _guild_command["name"] not in __check_guild_commands[_guild_id]:
                             self.__guild_commands[_guild_id]["clean"] = False
                             self.__guild_commands[_guild_id]["commands"].append(_guild_command)
