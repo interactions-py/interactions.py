38d69f911f21a6bd90bddaf859dbeae8a3166c27
LordOfPolls <ddavidallen13@gmail.com>
2023-01-18 18:57:02 +0000

    feat: amend main file
    this is a temporary file for testing, nothing more to see

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from interactions import Client, listen, File
+import logging
+import os
+
+from interactions import Client, listen, slash_command
+
+logging.basicConfig()
+logging.getLogger("interactions").setLevel(logging.DEBUG)
 
 bot = Client()
 
@@ -8,4 +14,14 @@ async def on_startup():
     print(f"Logged in as {bot.user}")
 
 
-bot.start("NzAxMzQ3ODc4MzExODIxMzcz.Gc0rqV.PH6XmX0VFBmQHZVieaAfNB6paUiThcSliqhbjk")
+@bot.event()
+async def on_ready():
+    print("Im ready!")
+
+
+@slash_command("ping")
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot.start(os.environ["TOKEN"])
