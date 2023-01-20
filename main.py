7c27a72c1f8b38f1e5afae2437e95e9c634921bd
LordOfPolls <ddavidallen13@gmail.com>
2023-01-20 06:06:24 +0000

    feat: add components to main.py example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import asyncio
 import logging
 import os
 
+import interactions
 from interactions import Client, listen, slash_command
 
 logging.basicConfig()
@@ -18,10 +20,23 @@ async def on_startup():
 async def on_ready():
     print("Im ready!")
 
+    while True:
+        await asyncio.sleep(5)
+        print(bot.latency)
+
 
 @slash_command("ping")
 async def ping(ctx):
-    await ctx.send("Pong!")
+    action_rows = [
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyles.DANGER,
+                label="Danger Button",
+            )
+        )
+    ]
+
+    await ctx.send("Pong!", components=action_rows)
 
 
 bot.start(os.environ["TOKEN"])
