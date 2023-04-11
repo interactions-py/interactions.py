e9371a2b5c7e91712ec47653060b7f5cb2c32479
LordOfPolls <dev@lordofpolls.com>
2023-04-11 09:22:42 +0100

    feat: add missing message types

--- interactions/models/discord/enums.py
+++ interactions/models/discord/enums.py
@@ -342,7 +342,11 @@ class PremiumType(CursedIntEnum):
 
 
 class MessageType(CursedIntEnum):
-    """Types of message."""
+    """
+    Types of message.
+
+    Ref: https://discord.com/developers/docs/resources/channel#message-object-message-types
+    """
 
     DEFAULT = 0
     RECIPIENT_ADD = 1
@@ -369,6 +373,12 @@ class MessageType(CursedIntEnum):
     CONTEXT_MENU_COMMAND = 23
     AUTO_MODERATION_ACTION = 24
     ROLE_SUBSCRIPTION_PURCHASE = 25
+    INTERACTION_PREMIUM_UPSELL = 26
+    STAGE_START = 27
+    STAGE_END = 28
+    STAGE_SPEAKER = 29
+    STAGE_TOPIC = 31
+    GUILD_APPLICATION_PREMIUM_SUBSCRIPTION = 32
 
 
 class EmbedType(Enum):
