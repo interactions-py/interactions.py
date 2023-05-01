e7e7d3e2a80a73820aa86224f607e1a1644b620a
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2023-05-01 18:09:15 +0100

    else + if = elif

--- interactions/api/events/processors/reaction_events.py
+++ interactions/api/events/processors/reaction_events.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING
 
 import interactions.api.events as events
 from interactions.models import PartialEmoji, Reaction
+
 from ._template import EventMixinTemplate, Processor
 
 if TYPE_CHECKING:
@@ -14,11 +15,10 @@ class ReactionEvents(EventMixinTemplate):
     async def _handle_message_reaction_change(self, event: "RawGatewayEvent", add: bool) -> None:
         if member := event.data.get("member"):
             author = self.cache.place_member_data(event.data.get("guild_id"), member)
+        elif guild_id := event.data.get("guild_id"):
+            author = await self.cache.fetch_member(guild_id, event.data.get("user_id"))
         else:
-            if guild_id := event.data.get("guild_id"):
-                author = await self.cache.fetch_member(guild_id, event.data.get("user_id"))
-            else:
-                author = await self.cache.fetch_user(event.data.get("user_id"))
+            author = await self.cache.fetch_user(event.data.get("user_id"))
 
         emoji = PartialEmoji.from_dict(event.data.get("emoji"))  # type: ignore
         message = self.cache.get_message(event.data.get("channel_id"), event.data.get("message_id"))
