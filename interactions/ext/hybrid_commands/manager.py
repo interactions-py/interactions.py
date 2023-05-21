3943c08756db4009b8a12922725512a26b82cb6c
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2023-05-21 18:16:46 -0400

    feat: add use_slash_command_msg argument

--- interactions/ext/hybrid_commands/manager.py
+++ interactions/ext/hybrid_commands/manager.py
@@ -1,22 +1,57 @@
-from typing import cast
+from typing import cast, Callable, Any
 
 from interactions import Client, BaseContext, listen
 from interactions.api.events import CallbackAdded, ExtensionUnload
 from interactions.ext import prefixed_commands as prefixed
 
 from .context import HybridContext
-from .hybrid_slash import _values_wrapper, base_subcommand_generator, HybridSlashCommand, slash_to_prefixed
+from .hybrid_slash import (
+    _values_wrapper,
+    base_subcommand_generator,
+    HybridSlashCommand,
+    _HybridToPrefixedCommand,
+    slash_to_prefixed,
+)
 
 __all__ = ("HybridManager", "setup")
 
 
+def add_use_slash_command_message(
+    prefixed_cmd: _HybridToPrefixedCommand, slash_cmd: HybridSlashCommand
+) -> _HybridToPrefixedCommand:
+    if prefixed_cmd.has_binding:
+
+        def wrap_old_callback(func: Callable) -> Any:
+            async def _msg_callback(self, ctx: prefixed.PrefixedContext, *args, **kwargs):
+                await ctx.reply(f"This command has been updated. Please use {slash_cmd.mention(ctx.guild_id)} instead.")
+                await func(ctx, *args, **kwargs)
+
+            return _msg_callback
+
+    else:
+
+        def wrap_old_callback(func: Callable) -> Any:
+            async def _msg_callback(ctx: prefixed.PrefixedContext, *args, **kwargs):
+                await ctx.reply(f"This command has been updated. Please use {slash_cmd.mention(ctx.guild_id)} instead.")
+                await func(ctx, *args, **kwargs)
+
+            return _msg_callback
+
+    prefixed_cmd.callback = wrap_old_callback(prefixed_cmd.callback)
+    return prefixed_cmd
+
+
 class HybridManager:
-    def __init__(self, client: Client, hybrid_context: type[BaseContext] = HybridContext) -> None:
+    def __init__(
+        self, client: Client, *, hybrid_context: type[BaseContext] = HybridContext, use_slash_command_msg: bool = False
+    ) -> None:
         if not hasattr(client, "prefixed") or not isinstance(client.prefixed, prefixed.PrefixedManager):
             raise TypeError("Prefixed commands are not set up for this bot.")
 
-        self.client = cast(prefixed.PrefixedInjectedClient, client)
         self.hybrid_context = hybrid_context
+        self.use_slash_command_msg = use_slash_command_msg
+
+        self.client = cast(prefixed.PrefixedInjectedClient, client)
         self.ext_command_list: dict[str, list[str]] = {}
 
         self.client.add_listener(self.on_callback_added.copy_with_binding(self))
@@ -31,6 +66,9 @@ class HybridManager:
 
         cmd = event.callback
         prefixed_transform = slash_to_prefixed(cmd)
+
+        if self.use_slash_command_msg:
+            prefixed_transform = add_use_slash_command_message(prefixed_transform, cmd)
 
         if cmd.is_subcommand:
             base = None
@@ -73,5 +111,7 @@ class HybridManager:
         del self.ext_command_list[event.extension.extension_name]
 
 
-def setup(client: Client, hybrid_context: type[BaseContext] = HybridContext) -> HybridManager:
-    return HybridManager(client, hybrid_context)
+def setup(
+    client: Client, *, hybrid_context: type[BaseContext] = HybridContext, use_slash_command_msg: bool = False
+) -> HybridManager:
+    return HybridManager(client, hybrid_context=hybrid_context, use_slash_command_msg=use_slash_command_msg)
